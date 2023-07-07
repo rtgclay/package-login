964d753fd6e2372411f0f690c0ad53d685fa31e7
Mar'i Fauzan <marifauzan20@gmail.com>
2023-07-07 13:25:02 +0700

    update new page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-<!-- resources/views/auth/login.blade.php -->
+{{-- <!-- resources/views/auth/login.blade.php -->
 <style>
     @charset "utf-8";
 
@@ -236,4 +236,23 @@
         </center>
     </div>
 
-</div>
+</div> --}}
+
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="bg-red-500">
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    </div>
+</body>
+
+</html>
